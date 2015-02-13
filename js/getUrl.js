b7b8a97ff1ec33672cabb8c25866b8e3cfad1e1c
fanset <fansetcool@gmail.com>
2015-02-13 22:13:35 +0800

    jun

--- js/getUrl.js
+++ js/getUrl.js
@@ -15,3 +15,17 @@ function getUrlParam(name){
               }
               return null;
             }
+
+function getCookie(name){
+	      var strCookie = document.cookie;
+          var arrCookie = strCookie.split("; ");
+          for (var i = 0; i < arrCookie.length; i++) {
+          var arr = arrCookie[i].split("=");
+          if (arr[0] == name) {
+        return arr[1];
+      }
+      else{
+      	return "";
+      }
+    }
+}
